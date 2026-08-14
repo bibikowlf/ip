183ee134c70b7048c8383e0a094f92f243056924
bibikowlf <bibikowolf@gmail.com>
2026-08-14 17:33:45 +0800

    Modify constant declarations
    Move constants to class level and add private static final modifiers

--- src/main/java/Laby.java
+++ src/main/java/Laby.java
@@ -3,19 +3,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Laby {
-    public static void main(String[] args) {
-        String banner = "#       ###   ####   #   #\n" +
-                "#      #   #  #   #   # #\n" +
-                "#      #####  ####     #\n" +
-                "#      #   #  #   #    #\n" +
-                "#####  #   #  ####     #\n";
-        String divider = "____________________________________________________________\n\n";
-        String openMsg = "Hello Chief. Laby is your personal assistant.\n";
-        String askMsg =  "What orders do you have today?\n";
-        String exitMsg = "Goodbye. Switching to rest mode.\n";
+    private static final String banner = "#       ###   ####   #   #\n" +
+            "#      #   #  #   #   # #\n" +
+            "#      #####  ####     #\n" +
+            "#      #   #  #   #    #\n" +
+            "#####  #   #  ####     #\n";
+    private static final String divider = "____________________________________________________________\n\n";
+    private static final String openMsg = "Hello Chief. Laby is your personal assistant.\n";
+    private static final String askMsg =  "What orders do you have today?\n";
+    private static final String exitMsg = "Goodbye. Switching to rest mode.\n";
 
-        List<String> tasks = new ArrayList<>();
+    private static final List<String> tasks = new ArrayList<>();
 
+    static void main(String[] args) {
         System.out.print(divider + banner + divider + openMsg + askMsg + divider);
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -24,7 +24,7 @@ public class Laby {
                 System.out.print(divider + exitMsg + divider);
                 break;
             } else if (input.equals("list")) {
-                System.out.println(divider);
+                System.out.print(divider);
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + ". " + tasks.get(i));
                 }
