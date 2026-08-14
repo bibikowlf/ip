7cd551eb0f0853aea804f9b1c76042ebc9b88af9
bibikowlf <bibikowolf@gmail.com>
2026-08-14 16:55:29 +0800

    Update to echo commands
    Echo commands and exit if the command is "bye"

--- src/main/java/Laby.java
+++ src/main/java/Laby.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Laby {
     public static void main(String[] args) {
         String banner = "#       ###   ####   #   #\n" +
@@ -6,9 +8,19 @@ public class Laby {
                 "#      #   #  #   #    #\n" +
                 "#####  #   #  ####     #\n";
         String divider = "____________________________________________________________\n\n";
-        String openMsg = "Hello Chief. Laby is your personal assistant.\nWhat orders do you have today?\n";
+        String openMsg = "Hello Chief. Laby is your personal assistant.\n";
+        String askMsg =  "What orders do you have today?\n";
         String exitMsg = "Goodbye. Switching to rest mode.\n";
 
-        System.out.println(divider + banner + divider + openMsg + divider + exitMsg);
+        System.out.println(divider + banner + divider + openMsg + askMsg + divider);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(divider + exitMsg + divider);
+                break;
+            }
+            System.out.println(divider + input + "\n" + divider);
+        }
     }
 }
