e949da86b584cfa1d1a32e69c88c5f328620ab18
bibikowlf <bibikowolf@gmail.com>
2026-08-15 16:32:05 +0800

    Add reply messages
    Add more detailed reply messages for list, mark, unmark

--- src/main/java/Laby.java
+++ src/main/java/Laby.java
@@ -12,6 +12,9 @@ public class Laby {
     private static final String openMsg = "Hello Chief. Laby is your personal assistant.\n";
     private static final String askMsg =  "What orders do you have today?\n";
     private static final String exitMsg = "Goodbye. Switching to rest mode.\n";
+    private static final String listMsg = "Here are the tasks in your list:\n";
+    private static final String markMsg = "Understood. Laby has marked the task as done.\n";
+    private static final String unmarkMsg = "Understood. Laby has marked the task as not done.\n";
     private static final List<Task> tasks = new ArrayList<>();
 
     private static void printTasks() {
@@ -30,16 +33,17 @@ public class Laby {
                 break;
             } else if (input.equals("list")) {
                 System.out.print(divider);
+                System.out.print(listMsg);
                 printTasks();
                 System.out.print(divider);
             } else if (input.startsWith("mark")) {
                 int taskId = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks.get(taskId).markAsDone();
-                System.out.print(divider + "  " + tasks.get(taskId).toString() + "\n" + divider);
+                System.out.print(divider + markMsg + "  " + tasks.get(taskId).toString() + "\n" + divider);
             } else if (input.startsWith("unmark")) {
                 int taskId = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks.get(taskId).markAsUndone();
-                System.out.print(divider + "  " + tasks.get(taskId).toString() + "\n" + divider);
+                System.out.print(divider + unmarkMsg + "  " + tasks.get(taskId).toString() + "\n" + divider);
             } else {
                 tasks.add(new Task(input));
                 System.out.print(divider + "added: " + input + "\n" + divider);
